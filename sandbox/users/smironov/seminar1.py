c933f226678622d5de6f0be5285fdaca4d0d2c0e
Sergey Mironov <grrwlf@gmail.com>
2017-02-27 20:13:00 +0300

    add users/smironov

---
+++ sandbox/users/smironov/seminar1.py
@@ -0,0 +1,10 @@
+import numpy as np
+m = {v:k for k,v in list(enumerate(set(' '.join(['aaa sdas','bbb asdas','ccc asdasd']).split())))}
+
+print(m)
+
+X = np.array ([[1001,1002],[2001,2002]])
+a=[0,0]
+b=[0,1]
+
+
